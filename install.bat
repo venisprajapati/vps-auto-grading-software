d680e73d1b1546cb980636351bf34958a436ce8e
Venis Prajapati <59964517+venisprajapati@users.noreply.github.com>
2021-09-21 13:32:49 +0530

    Updated install.bat - in v 1.0.1 - # 6.5

--- install.bat
+++ install.bat
@@ -1,5 +1,10 @@
 @echo off
 python -m venv env
 CALL env/Scripts/activate.bat
+python -m pip install --upgrade pip
+mkdir uploads
+cd uploads
+mkdir omrs
+cd ..
 "env/Scripts/pip.exe" install -r requirements.txt
 pause
