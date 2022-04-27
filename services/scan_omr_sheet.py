1ede70f18d5cc982775ee5d0765448f98dd763d0
Venis Prajapati <venisprajapati2102@gmail.com>
2022-04-27 18:08:15 +0530

    Updated for 200 mcqs

--- services/scan_omr_sheet.py
+++ services/scan_omr_sheet.py
@@ -13,7 +13,9 @@ def ScanOmrs(total_mcqs):
 
     id_point = (21, 50)
     start_point = [(21, 221), (134, 50), (134, 221), (247, 50), (247, 221), (360, 50), (360, 221), (473, 50), (473, 221),
-                (21, 448), (21, 619), (134, 448), (134, 619), (247, 448), (247, 619), (360, 448), (360, 619), (473, 448), (473, 619)]
+               (21, 410), (21, 581), (134, 410), (134, 581), (247, 410), (247, 581), (360, 410), (360, 581), (473, 410), (473, 581),
+               (21, 772), (134, 772), (247, 772), (360, 772), (473, 772)]
+    total_bubble_count_in_column = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 2, 2, 2, 2, 2]
 
     ScannedOmrs = []
 
@@ -49,7 +51,9 @@ def ScanOmrs(total_mcqs):
             start_x = start_point[pts][0]
             start_y = start_point[pts][1]
 
-            for box in range(0, 10):
+            total_bubble_count = total_bubble_count_in_column[pts]
+
+            for box in range(0, total_bubble_count):
 
                 if Counter <= total_mcqs:
 
--- test2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-CALL env/Scripts/activate.bat
-"env/Scripts/python.exe" test2.py %*
-pause
